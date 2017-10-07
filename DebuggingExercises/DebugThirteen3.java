56097bb323fcc4be42ba481a24c8f0838e3d2684
Jonah Burnaby <jonah.burnaby@my.jcu.edu.au>
2017-10-07 18:03:46 +1000

    Debug 13s donezo

--- DebuggingExercises/DebugThirteen3.java
+++ DebuggingExercises/DebugThirteen3.java
@@ -8,8 +8,8 @@ public class DebugThirteen3
    public static void main(String[] args)
    {
       Path file =
-         Paths.get("C:\\Java\\Chapter.13\\DebugData4.txt");
-      Scanner kb = new Scanner();
+              Paths.get("C:\\Java\\Chapter.13\\DebugData4.txt");
+      Scanner kb = new Scanner(System.in);
       String[] array = new String[2];
       String string = "";
       String delimiter = ",";
@@ -20,23 +20,23 @@ public class DebugThirteen3
       {
          OutputStream output = new BufferedOutputStream(Files.newOutputStream(file));
          BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output));
-         System.out.print("Enter first entree or " + QUIT + " to quit >> ");        
+         System.out.print("Enter first entree or " + QUIT + " to quit >> ");
          entree = kb.nextLine();
          while(!entree.equals(QUIT))
          {
             System.out.print("Enter price >> ");
             price = kb.nextLine();
             string = entree + delimiter + price +
-              System.getProperty("line.separator");
+                    System.getProperty("line.separator");
             writer.write(string, 0, string.length());
-            System.out.print("Enter next entree or " + QUIT + " to quit >> ");        
+            System.out.print("Enter next entree or " + QUIT + " to quit >> ");
             entree = kb.nextLine();
          }
          writer.close();
       }
       catch(Exception e)
       {
-        System.out.println("Message: " + e);
+         System.out.println("Message: " + e);
       }
    }
 }
